fa3195da741e218db311fb8d29b7fad663b3ffe5
ivonapavela <92436197+ivonapavela@users.noreply.github.com>
2024-01-21 16:14:17 +0100

    11

---
+++ semester-project/app/myProfile/page.tsx
@@ -0,0 +1,8 @@
+export default function MyProfile() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-10">
+        <h1 className="text-3xl font-bold">Log In Page</h1>
+      </main>
+    );
+  }
+  
