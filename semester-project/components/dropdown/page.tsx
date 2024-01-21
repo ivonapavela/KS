1a7586630deddc2bc87fa1701fc7dc9d859833d2
ivonapavela <92436197+ivonapavela@users.noreply.github.com>
2024-01-21 20:47:37 +0100

    20

--- semester-project/components/dropdown/page.tsx
+++ semester-project/components/dropdown/page.tsx
@@ -1,6 +1,5 @@
 import Button from '@/components/button/page';
 import React, { useState } from 'react';
-import "./dropdown.css";
 
 interface DropdownProps {
   showDropdown: boolean;
@@ -12,10 +11,30 @@ const categories = ["breakfast", "lunch", "dinner", "dessert", "snack", "all"];
 
 const Dropdown: React.FC<DropdownProps> = ({ showDropdown, onMouseOver, onMouseOut }) => {
   const [clickedButton, setClickedButton] = useState<string>('');
+  const dropdownStyle: React.CSSProperties = {
+    position: 'absolute',
+    top: '100%', // Increased margin-top
+    left: 0,
+    width: '150px',
+    backgroundColor: '#FFFFFF',
+    borderRadius: '20px', // Increased border-radius for a rounder appearance
+    padding: '8px',
+    display: showDropdown ? 'block' : 'none',
+    zIndex: '1000',
+  };
+
+  const listStyle: React.CSSProperties = {
+    listStyleType: 'none',
+    padding: 0,
+    margin: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
 
   return (
-    <div id="dropdown-container" onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
-      <ul id="dropdown-list">
+    <div style={dropdownStyle} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+      <ul style={listStyle}>
         {categories.map((name) => (
           <li key={name}>
             {name === "all" ? (
