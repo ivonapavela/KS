8271788405d0285bf18314efaa1ffebed7c4f199
ivonapavela <92436197+ivonapavela@users.noreply.github.com>
2024-01-21 16:00:02 +0100

    8

--- semester-project/app/MyProfile/page.tsx
+++ semester-project/app/MyProfile/page.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import './login.css';
+import React  from 'react';
+import './LoginPage.css';
 
-const LogInPage: React.FC = () => {
+const LoginPage: React.FC = () => {
+
   return (
-    <div className="login-page">
-      <div className="login-container">
-        <h1 className="login-heading">Log In</h1>
-        <div className="input-container">
-          <input type="text" placeholder="Username" className="username-input" />
-        </div>
-        <div className="input-container">
-          <input type="password" placeholder="Password" className="password-input" />
-        </div>
-      </div>
+    <div>
+      hahaha
     </div>
   );
 };
 
-export default LogInPage;
+export default LoginPage;
